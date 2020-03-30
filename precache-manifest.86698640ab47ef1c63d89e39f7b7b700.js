50a3ac1c99562226200853c999a7f7b849aca482
garkanum3 <gabriel@moquality.com>
2020-03-29 22:30:46 -0400

    Updates

---
+++ precache-manifest.86698640ab47ef1c63d89e39f7b7b700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf5762af684c7951271e9d59248509c",
+    "url": "/gmail-redesigned/index.html"
+  },
+  {
+    "revision": "cdc90a9c8e62e1c64ea1",
+    "url": "/gmail-redesigned/static/css/main.dedd9dfa.chunk.css"
+  },
+  {
+    "revision": "aa4c1bd069e69a6b9f42",
+    "url": "/gmail-redesigned/static/js/2.c556cb45.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gmail-redesigned/static/js/2.c556cb45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc90a9c8e62e1c64ea1",
+    "url": "/gmail-redesigned/static/js/main.bdb3db5f.chunk.js"
+  },
+  {
+    "revision": "38bfbb27c6747b6668eb",
+    "url": "/gmail-redesigned/static/js/runtime-main.2266fde8.js"
+  }
+]);
